1a13bd7e3d06dab2357dcbe24c2620230c2d3ff9
quaggosxr <arthur.quaggos200@gmail.com>
2026-01-29 10:52:05 -0300

    Add files via upload

---
+++ entradaDeDados.java
@@ -0,0 +1,13 @@
+package aula2;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class entradaDeDados {
+    static void main() {
+        Scanner banana = new Scanner(System.in);
+
+       String nome = banana.next();
+
+        IO.println(nome);
+    }
+}
